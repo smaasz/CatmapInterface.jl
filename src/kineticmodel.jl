ed7d688ec379a501973923836fb56ec68de09f88
Steffen Maass <maassstef@gmail.com>
2023-06-17 18:04:22 +0200

    first version

--- src/kineticmodel.jl
+++ src/kineticmodel.jl
@@ -10,24 +10,39 @@ function get_kinetic_model(setup_file_path, energies_file_path)
     cd(tmpdir)
     tmp_setup_file_path     = cp(setup_file_path, joinpath(tmpdir, setup_file_name))
     tmp_energies_file_path  = cp(energies_file_path, joinpath(tmpdir, energies_file_name))
-    # do things
-    println("test")
-    println(tmp_setup_file_path)
-    a = py"catmap_kinetic_model"(tmp_setup_file_path)
+
+    grid, kin_model = py"catmap_kinetic_model"(tmp_setup_file_path)
+
+    grid = ((x) -> convert_pythonfloat.(x)).(grid)
 
     cd(workingdir)
 
-    return a
+    return grid, kin_model
 
 end
 
+function convert_pythonfloat(x)
+    return Float64(BigFloat(pycall(py"str", String, x)))
+end
+
 function convert_to_julia(kin_model)
+
+    # verify that the kinetic model fulfills the assumptions
+
+    # remove last three lines
+    kin_model = join(split(kin_model, "\n")[1:end-5], "\n") * "end"
     
-    kin_model = replace(kin_model, r"def (.+):" => s"function \1") * "end"
+    #kin_model = replace(kin_model, r"def (?<functionname>.+?)\((?<arguments>.*)\):" => s"function \g<functionname>!(dtheta_dt, \g<arguments>)")
+    kin_model = replace(kin_model, r".*def.*" => s"function kinetic_model!(dtheta_dt::AbstractVector{T}, theta::AbstractVector{T}, ps, t) where T\n\n    kf, kr, p = ps\n")
+    
+    kin_model = replace(kin_model, r"dtheta_dt = .*" => s"")
+    
     kin_model = replace(kin_model, r"(\w+?)\[(\d*?)\]" => s"\1[\2 + 1]")
     kin_model = replace(kin_model, r"mpf\('(.*)'\)" => s"\1")
-    kin_model = replace(kin_model, r"(\h)\[(.*?)\]\h*\*\h*(.*?)([\h|\n])" => s"\1\2 * ones(\3)\4")
+    #kin_model = replace(kin_model, r"mpf\('(.*)'\)" => s"BigFloat('\1')")
+    kin_model = replace(kin_model, r"(\h)\[(.*?)\]\h*\*\h*(.*?)([\h|\n])" => s"\1\2 * ones(T, \3)\4")
     kin_model = replace(kin_model, r"([^a-zA-Z])len\((.*?)\)" => s"\1length(\2)")
 
-    return kin_model
+    #return kin_model
+    return eval(Meta.parse(kin_model))
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,16 @@
-using CatmapInterface
+using .CatmapInterface
 using Test
+using DifferentialEquations
 
-@testset "CatmapInterface.jl" begin
+(grid, kinetic_model)   = get_kinetic_model("/home/steffen/Documents/masterarbeit/catmap-1/tutorials/2-creating_microkinetic_model/CO_oxidation.mkm", "/home/steffen/Documents/masterarbeit/catmap-1/tutorials/2-creating_microkinetic_model/energies.txt")
+kinetic_model!          = convert_to_julia(kinetic_model)
+
+
+θ       = [0.4, 0.4]
+prob    = SteadyStateProblem(kinetic_model!, θ, p = (grid[1,1], grid[1,2], [1.0, 1.0, 1.0])) 
+sol     = solve(prob, DynamicSS(QNDF()))
+println(sol)
+
+#@testset "CatmapInterface.jl" begin
     # Write your tests here.
-end
+#end
